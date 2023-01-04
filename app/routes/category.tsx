03bc38a3f3677dc17cb9b59abd0a8c825044202f
obrunosanches <obrunosanches@gmail.com>
2023-01-04 07:22:18 -0300

    feat: create category and category type

--- app/routes/category.tsx
+++ app/routes/category.tsx
@@ -5,26 +5,48 @@ import type {
 } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
-import { Form, Link, useLoaderData } from "@remix-run/react";
+import {
+  Form,
+  Link,
+  Outlet,
+  useLoaderData,
+  useNavigate,
+} from "@remix-run/react";
 import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { Button, Label, TextInput, Select } from "flowbite-react";
 
-import type { CategoryType as ICategoryType } from "@prisma/client";
+import type {
+  Category as ICategory,
+  CategoryType as ICategoryType,
+} from "@prisma/client";
 
 import { prisma } from "~/utils/prisma.server";
 
+interface ICategoryWithType extends ICategory {
+  type: ICategoryType;
+}
+
 export const loader: LoaderFunction = async () => {
+  const category = await prisma.category.findMany({
+    include: {
+      type: true,
+    },
+  });
+
   const categoryTypes = await prisma.categoryType.findMany();
 
-  return json({ categoryTypes });
+  return json({ category, categoryTypes });
 };
 
 export const action: ActionFunction = async ({ request }: ActionArgs) => {
   const formData = await request.formData();
-  const categoryData = Object.fromEntries(formData);
+  const name = formData.get("name");
+  const typeId = formData.get("typeId");
 
-  await prisma.categoryType.create({
+  await prisma.category.create({
     data: {
-      description: String(categoryData.description),
+      name: String(name),
+      typeId: String(typeId),
     },
   });
 
@@ -32,10 +54,13 @@ export const action: ActionFunction = async ({ request }: ActionArgs) => {
 };
 
 export default function Category() {
-  const { categoryTypes } = useLoaderData();
+  const navigate = useNavigate();
+  const { category, categoryTypes } = useLoaderData();
 
   return (
     <div className="px-5 mt-5">
+      <Outlet />
+
       <div className="flex justify-between items-center">
         <h1 className="text-xl">Categorias</h1>
         <Link to="/">Voltar</Link>
@@ -44,27 +69,27 @@ export default function Category() {
       <div className="flex justify-center mt-5">
         <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
           <Form method="post">
-            <div className="mb-6">
-              <label
-                htmlFor="description"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Descrição
-              </label>
-              <input
-                name="description"
-                type="text"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                required
-              />
+            <Label htmlFor="name" value="Descrição" />
+            <TextInput id="name" name="name" required />
+
+            <div className="mt-4 flex items-end">
+              <div className="flex-auto mr-4">
+                <Label htmlFor="typeId" value="Tipo" />
+                <Select id="typeId" name="typeId" required>
+                  <option value="">Selecione o tipo</option>
+                  {categoryTypes.map((item: ICategoryType) => {
+                    return (
+                      <option key={item.id} value={item.id}>
+                        {item.description}
+                      </option>
+                    );
+                  })}
+                </Select>
+              </div>
+              <Button onClick={() => navigate("/category/type")}>Tipo</Button>
             </div>
-            <div className="flex justify-end">
-              <button
-                type="submit"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Adicionar
-              </button>
+            <div className="flex justify-end mt-4">
+              <Button type="submit">Salvar</Button>
             </div>
           </Form>
         </div>
@@ -78,18 +103,22 @@ export default function Category() {
                 Descrição
               </th>
               <th scope="col" className="py-3 px-6">
+                Tipo
+              </th>
+              <th scope="col" className="py-3 px-6">
                 Ações
               </th>
             </tr>
           </thead>
           <tbody>
-            {categoryTypes.map((item: ICategoryType) => {
+            {category.map((item: ICategoryWithType) => {
               return (
                 <tr
                   key={item.id}
                   className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
                 >
-                  <td className="py-4 px-6">{item.description}</td>
+                  <td className="py-4 px-6">{item.name}</td>
+                  <td className="py-4 px-6">{item.type.description}</td>
                   <td className="py-4 px-6" colSpan={2}>
                     <div className="flex">
                       <PencilSquareIcon height={18} />
