92d74c74c935a04764dfa4ccc0924f2f4f2614d3
obrunosanches <obrunosanches@gmail.com>
2023-01-05 09:46:48 -0300

    feat: create layout component

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,40 +1,7 @@
-import { Navbar, Sidebar } from "flowbite-react";
-import {
-  ChartPieIcon,
-  BuildingLibraryIcon,
-  TagIcon,
-} from "@heroicons/react/24/outline";
+import type { LoaderFunction} from "@remix-run/node";
+import { redirect } from "@remix-run/node"
 
-import logo from "~/assets/images/logo.svg";
 
-export default function Index() {
-  return (
-    <>
-      <Navbar fluid={true} rounded={true}>
-        <Navbar.Brand>
-          <img src={logo} className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-            Control Money
-          </span>
-        </Navbar.Brand>
-      </Navbar>
-      <div className="w-fit">
-        <Sidebar aria-label="Sidebar with options to control pages">
-          <Sidebar.Items>
-            <Sidebar.ItemGroup>
-              <Sidebar.Item icon={TagIcon} href="/category">
-                Category
-              </Sidebar.Item>
-              <Sidebar.Item icon={BuildingLibraryIcon} href="#">
-                Account
-              </Sidebar.Item>
-              <Sidebar.Item href="#" icon={ChartPieIcon}>
-                Transações
-              </Sidebar.Item>
-            </Sidebar.ItemGroup>
-          </Sidebar.Items>
-        </Sidebar>
-      </div>
-    </>
-  );
+export const loader: LoaderFunction = () => {
+  return redirect('/dashboard')
 }
