5454698e1e4fabf894f43fb3b847eee40bc98864
Bruno Sanches <obrunosanches@gmail.com>
2023-01-09 14:01:59 -0300

    feat: create navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<script setup lang='ts'>
+import { computed } from 'vue';
+import classNames from 'classnames'
+
+const props = defineProps({
+  className: {
+    type: String,
+    default: ''
+  },
+  sticky: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const navbarBaseClasses = 'px-2 sm:px-4 py-2.5 dark:bg-gray-900'
+const navbarFloatClasses = 'fixed w-full top-0 left-0 border-b border-gray-700'
+const navbarClasses = computed(() => {
+  return classNames(
+    navbarBaseClasses,
+    props.className,
+    props.sticky && navbarFloatClasses
+  )
+})
+</script>
+
+<template>
+  <nav :class="navbarClasses">
+    <div class="container flex flex-wrap items-center">
+      <slot />
+    </div>
+  </nav>
+</template>
