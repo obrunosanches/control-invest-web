72b2a483ed70ac71bf7090ab5edc65045e571107
obrunosanches <obrunosanches@gmail.com>
2023-01-09 15:54:13 -0300

    feat: create sidebar component

---
+++ src/components/Sidebar/Sidebar.Item.vue
@@ -0,0 +1,16 @@
+<script setup lang='ts'>
+const props = defineProps({
+  href: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<template>
+  <li>
+    <a :href="props.href" class="flex items-center p-2 text-base font-normal rounded-lg text-white hover:bg-gray-700">
+      <slot />
+    </a>
+  </li>
+</template>
