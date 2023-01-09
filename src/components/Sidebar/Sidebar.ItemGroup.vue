04b98a3c7139841472fd57c303ff32116e50936b
Bruno Sanches <obrunosanches@gmail.com>
2023-01-09 15:54:13 -0300

    feat: create sidebar component

---
+++ src/components/Sidebar/Sidebar.ItemGroup.vue
@@ -0,0 +1,14 @@
+<script setup lang='ts'>
+const props = defineProps({
+  className: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<template>
+  <ul :class="props.className">
+    <slot />
+  </ul>
+</template>
---
+++ src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,20 @@
+<script setup lang='ts'>
+const props = defineProps({
+  className: {
+    type: String,
+    default: ''
+  },
+  label: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<template>
+  <aside :class="props.className" :aria-label="props.label">
+    <div class="px-3 py-4 overflow-y-auto">
+      <slot />
+    </div>
+  </aside>
+</template>
