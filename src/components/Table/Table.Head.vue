36432dd26d2a0f866709e203991d0f132d70d8d8
obrunosanches <obrunosanches@gmail.com>
2023-01-10 22:19:51 -0300

    feat: create table component

---
+++ src/components/Table/Table.Head.vue
@@ -0,0 +1,16 @@
+<script setup lang='ts'>
+const props = defineProps({
+  className: {
+    type: String,
+    default: '',
+  }
+})
+</script>
+
+<template>
+  <thead :class="props.className">
+    <tr>
+      <slot></slot>
+    </tr>
+  </thead>
+</template>
---
+++ src/components/Table/Table.Row.vue
@@ -0,0 +1,24 @@
+<script setup lang='ts'>
+import { computed, inject } from 'vue';
+import classNames from 'classnames';
+
+const isStriped = inject('striped')
+const isHoverable = inject('hoverable')
+
+const baseClasses = 'bg-gray-800 [&:not(:last-child)]:border-gray-700 hover:bg-gray-600'
+const stripedClasses = 'odd:bg-gray-800 even:bg-gray-700'
+const hoverableClasses = 'hover:bg-gray-600'
+
+const className = computed(() => {
+  return classNames(baseClasses, {
+    [stripedClasses]: isStriped,
+    [hoverableClasses]: isHoverable,
+  })
+})
+</script>
+
+<template>
+  <tr :class="className">
+    <slot></slot>
+  </tr>
+</template>
