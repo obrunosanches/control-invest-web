d189094399d3c293565ab31dc9940d273f64f5c6
obrunosanches <obrunosanches@gmail.com>
2023-01-11 21:26:16 -0300

    refactor: add named slot in sidebar

--- src/components/Sidebar/TheSidebar.Item.vue
+++ src/components/Sidebar/TheSidebar.Item.vue
@@ -10,7 +10,9 @@ const props = defineProps({
 <template>
   <li>
     <a :href="props.href" class="flex items-center p-2 text-base font-normal rounded-lg text-white hover:bg-gray-700">
+      <slot name="before"></slot>
       <slot></slot>
+      <slot name="after"></slot>
     </a>
   </li>
 </template>
