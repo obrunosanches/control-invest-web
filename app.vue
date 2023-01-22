7d461dd338b61cf9eb12707f3ad14b287a817b2f
obrunosanches <obrunosanches@gmail.com>
2023-01-22 15:14:27 -0300

    style(TAILWIND): config tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="bg-slate-800 h-screen flex justify-center items-center">
+    <h1 class="text-gray-300 text-3xl font-bold">
+      Nuxt 3 with tailwind css!
+    </h1>
   </div>
 </template>
