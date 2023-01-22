d8abc68e2f5127058d2acb98d5112fbdef00e1d8
Bruno Sanches <obrunosanches@gmail.com>
2023-01-22 18:43:00 -0300

    feat: add basic structure buttom component

---
+++ components/TheButton.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useAttrs } from 'vue';
+
+const baseClass = "text-white bg-blue-600 hover:bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 focus:ring-blue-800"
+const className = useAttrs().class || baseClass
+</script>
+
+<template>
+  <button :class="className">
+    <slot />
+  </button>
+</template>
+
