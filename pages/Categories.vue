1532f8a4d4ae2b5d313cb6483dbd6804d057eaf2
Bruno Sanches <obrunosanches@gmail.com>
2023-01-22 18:52:34 -0300

    refactor: struture categories routing

--- pages/Categories.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <h1 class="text-gray-300 text-3xl font-bold">
-    Categorias
-  </h1>
-</template>
---
+++ pages/categories/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Button from '~/components/TheButton.vue';
+</script>
+
+<template>
+  <h2 class="text-2xl">Categorias</h2>
+
+  <div class="flex justify-end">
+    <NuxtLink to="categories/new">
+      <Button  type="button" color="default">
+        Adicionar
+      </Button>
+    </NuxtLink>
+  </div>
+</template>
