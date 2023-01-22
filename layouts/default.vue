0b2c2e9ea9ccc28d5be8057ae8e9fb2e840414a8
Bruno Sanches <obrunosanches@gmail.com>
2023-01-22 15:41:52 -0300

    feat: create layout structure default

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="bg-slate-800 h-screen flex justify-center items-center">
-    <h1 class="text-gray-300 text-3xl font-bold">
-      Nuxt 3 with tailwind css!
-    </h1>
+    <slot />
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 class="text-gray-300 text-3xl font-bold">
+    Nuxt 3 with tailwind css!
+  </h1>
+</template>
