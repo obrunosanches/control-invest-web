a5db4f55e95e63d09d51478909405d02ee745ff3
obrunosanches <obrunosanches@gmail.com>
2023-01-22 14:43:05 -0300

    feat: add base to icon component

--- src/components/Icons/IconAccount.vue
+++ src/components/Icons/IconAccount.vue
@@ -1,8 +1,10 @@
 <template>
-  <svg aria-hidden="true"
-    class="hidden md:block mr-3 flex-shrink-0 w-6 h-6 transition duration-75 group-hover:text-white"
-    stroke="currentColor" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <BaseIcon>
     <path strokeLinecap="round" strokeLinejoin="round"
       d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z" />
-  </svg>
+  </BaseIcon>
 </template>
+
+<script setup lang='ts'>
+import BaseIcon from './BaseIcon.vue';
+</script>
--- src/components/Icons/IconCategory.vue
+++ src/components/Icons/IconCategory.vue
@@ -1,9 +1,11 @@
 <template>
-  <svg aria-hidden="true"
-    class="hidden md:block mr-3 flex-shrink-0 w-6 h-6 transition duration-75 group-hover:text-white"
-    stroke="currentColor" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <BaseIcon>
     <path strokeLinecap="round" strokeLinejoin="round"
       d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z" />
     <path strokeLinecap="round" strokeLinejoin="round" d="M6 6h.008v.008H6V6z" />
-  </svg>
+  </BaseIcon>
 </template>
+
+<script setup lang="ts">
+import BaseIcon from './BaseIcon.vue';
+</script>
