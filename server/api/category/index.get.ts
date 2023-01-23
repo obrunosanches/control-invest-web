ca4a4dc464a1215dbf6b249aec7a0314d1b61166
obrunosanches <obrunosanches@gmail.com>
2023-01-23 20:35:03 -0300

    feat(SERVER): add route to load categories by api server

---
+++ server/api/category/index.get.ts
@@ -0,0 +1,11 @@
+import { prisma } from "~/server/utils/prisma.server";
+
+export default defineEventHandler(async () => {
+  const categories = await prisma.category.findMany({
+    include: {
+      type: true,
+    },
+  });
+  
+  return { categories }
+})
