ad5c11f48bc6342041c1d03345ab73901de89de7
obrunosanches <obrunosanches@gmail.com>
2023-01-24 20:22:01 -0300

    feat(COMPONENTS): add form components label, input and select

---
+++ components/Form/FormInput.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { useAttrs } from 'vue';
+import classNames from 'classnames';
+
+import Label from './FormLabel.vue';
+
+const props = defineProps({
+  label: {
+    type: String
+  }
+})
+
+const idElement = String(useAttrs().name) ?? ''
+const inputBaseClasses = 'rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+const inputClasses = computed(() => {
+  return classNames(
+    inputBaseClasses, {
+    [String(useAttrs().class)]: useAttrs().class
+  })
+})
+</script>
+
+<template>
+  <Label :for="idElement" :v-if="props.label">
+    {{ props.label }}
+  </Label>
+
+  <input :id="idElement" :class="inputClasses" autocomplete="off" v-bind="$attrs">
+</template>
---
+++ components/Form/FormLabel.vue
@@ -0,0 +1,5 @@
+<template>
+  <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" v-if="$attrs.for">
+    <slot />
+  </label>
+</template>
