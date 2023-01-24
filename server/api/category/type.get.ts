c8b2d4195223451873f2526ee2d5b5937269bc00
obrunosanches <obrunosanches@gmail.com>
2023-01-24 20:18:49 -0300

    refactor(API): change return api catetory type object to array

--- server/api/category/type.get.ts
+++ server/api/category/type.get.ts
@@ -3,5 +3,5 @@ import { prisma } from "~/server/utils/prisma.server";
 export default defineEventHandler(async () => {
   const categoryTypes = await prisma.categoryType.findMany();
 
-  return { categoryTypes }
+  return categoryTypes
 })
