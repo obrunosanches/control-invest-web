a329505edb62322e1ed331122576f283cbe2f76d
Bruno Sanches <obrunosanches@gmail.com>
2023-01-26 20:53:02 -0300

    feat(COMPONENTS): add table to list resources

---
+++ components/Table/BaseTable.vue
@@ -0,0 +1,28 @@
+<script setup lang='ts'>
+import { provide } from 'vue';
+
+const props = defineProps({
+  striped: {
+    type: Boolean,
+    default: false,
+  },
+  stripedColumns: {
+    type: Boolean,
+    default: false,
+  },
+  hoverable: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+provide('striped', props.striped)
+provide('stripedColumns', props.stripedColumns)
+provide('hoverable', props.hoverable)
+</script>
+
+<template>
+  <table v-bind="$attrs">
+    <slot></slot>
+  </table>
+</template>
