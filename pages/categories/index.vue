01b6978a7b656200cdec0312e28ac8b86a2586e4
Bruno Sanches <obrunosanches@gmail.com>
2023-01-26 20:55:02 -0300

    refactor: list categories with table component

--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -3,6 +3,12 @@ import { onMounted, ref } from 'vue'
 import type { Category as ICategory } from "@prisma/client";
 
 import Button from '~/components/TheButton.vue';
+import BaseTable from '~/components/Table/BaseTable.vue';
+import BaseTableHead from '~/components/Table/BaseTable.Head.vue';
+import BaseTableHeadCell from '~/components/Table/BaseTable.HeadCell.vue';
+import BaseTableBody from '~/components/Table/BaseTable.Body.vue';
+import BaseTableRow from '~/components/Table/BaseTable.BodyRow.vue';
+import BaseTableCell from '~~/components/Table/BaseTable.BodyCell.vue';
 
 const categories: any = ref<ICategory[]>([])
 
@@ -23,9 +29,22 @@ onMounted(async () => {
     </NuxtLink>
   </div>
 
-  <ul>
-    <li v-for="item in categories" :key="item.name">
-      {{ item.name }}
-    </li>
-  </ul>
+  <BaseTable v-if="categories.length" class="w-full text-sm text-left text-gray-400 mt-8" striped>
+    <BaseTableHead class="text-xs text-gray-400 uppercase bg-gray-700">
+      <BaseTableHeadCell>Name</BaseTableHeadCell>
+      <BaseTableHeadCell><span class="sr-only">actions</span></BaseTableHeadCell>
+    </BaseTableHead>
+    <BaseTableBody>
+      <BaseTableRow v-for="item in categories" :key="item.name">
+        <BaseTableCell>
+          {{ item.name }}
+        </BaseTableCell>
+        <BaseTableCell>
+          <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+          <span class="px-2 text-gray-300">|</span>
+          <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Remove</a>
+        </BaseTableCell>
+      </BaseTableRow>
+    </BaseTableBody>
+  </BaseTable>
 </template>
