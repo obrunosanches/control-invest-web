5a3a4dccf091682a2c72b8f9d1203141657bd8e5
obrunosanches <obrunosanches@gmail.com>
2023-01-27 22:10:27 -0300

    feat(COMPONENTS): add icons to control actions

---
+++ components/Icons/AddRegisttryIcon.vue
@@ -0,0 +1,9 @@
+<script setup lang='ts'>
+import BaseIcon from './BaseIcon.vue';
+</script>
+
+<template>
+  <BaseIcon>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+  </BaseIcon>
+</template>
