284688c4ef44c1615a8430b8a90acac70e02e147
obrunosanches <obrunosanches@gmail.com>
2023-01-29 16:49:06 -0300

    style: adjust visual form components

--- components/Form/FormSelect.vue
+++ components/Form/FormSelect.vue
@@ -11,7 +11,7 @@ const props = defineProps({
 })
 
 const idElement = String(useAttrs().name) ?? ''
-const selectBaseClasses = 'rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+const selectBaseClasses = 'border rounded-lg w-full p-2.5 text-sm dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
 const inputClasses = computed(() => {
   return classNames(
     selectBaseClasses, {
