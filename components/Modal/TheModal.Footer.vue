bb63137cf0394db11964024dc5bf0e0e0f4fc75b
Bruno Sanches <obrunosanches@gmail.com>
2023-01-29 17:50:24 -0300

    feat(COMPONENTS): add modal component

---
+++ components/Modal/TheModal.Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="p-6 rounded-b border-t dark:border-gray-600">
+    <slot />
+  </div>
+</template>
---
+++ components/Modal/TheModal.Header.vue
@@ -0,0 +1,27 @@
+<script setup lang='ts'>
+import CloseIcon from '~/components/Icons/CloseIcon.vue'
+
+const props = defineProps({
+  target: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String
+  }
+})
+</script>
+
+<template>
+  <div class="flex items-center justify-between rounded-t p-5 border-b dark:border-gray-600" aria-labelledby="modal-title">
+    <h3 :v-if="props.title" class="text-xl font-medium dark:text-white">
+      {{ props.title }}
+    </h3>
+    <button type="button" v-modal-hide="props.target"
+      class="text-sm ml-auto flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
+      <slot>
+        <CloseIcon class="h-4 w-4" viewBox="0 0 20 20" />
+      </slot>
+    </button>
+  </div>
+</template>
