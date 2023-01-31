d35d8ba63319cb592d98bd0d773c96b91de7ab6e
obrunosanches <obrunosanches@gmail.com>
2023-01-30 21:52:22 -0300

    feat: list sub categories by category

--- pages/categories/[id].vue
+++ pages/categories/[id].vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
-import type { Category as ICategory } from "@prisma/client";
+import type { Category as ICategory, SubCategory as ISubCategory } from "@prisma/client";
 
 import { hide } from '~/plugins/modal'
 
@@ -10,6 +10,14 @@ import ModalHeader from '~/components/Modal/TheModal.Header.vue';
 import ModalBody from '~/components/Modal/TheModal.Body.vue';
 import ModalFooter from '~/components/Modal/TheModal.Footer.vue';
 import FormInput from '~/components/Form/FormInput.vue';
+import BaseTable from '~/components/Table/BaseTable.vue';
+import BaseTableHead from '~/components/Table/BaseTable.Head.vue';
+import BaseTableHeadCell from '~/components/Table/BaseTable.HeadCell.vue';
+import BaseTableBody from '~/components/Table/BaseTable.Body.vue';
+import BaseTableRow from '~/components/Table/BaseTable.BodyRow.vue';
+import BaseTableCell from '~/components/Table/BaseTable.BodyCell.vue';
+import EditRegisttryIcon from '~/components/Icons/EditRegisttryIcon.vue';
+import RemoveRegisttryIcon from '~/components/Icons/RemoveRegisttryIcon.vue';
 
 // TODO: Use pinia to control store
 const route = useRoute()
@@ -17,13 +25,14 @@ const { id: categoryId } = route.params
 const modalId: string = "defaultModal"
 
 const { data: catogory } = await useAsyncData<ICategory>('categoryByID', () => $fetch(`/api/category/${categoryId}`))
+const { data: subCategories } = await useAsyncData<ISubCategory[]>('subCategories', () => $fetch(`/api/category/sub-category/${categoryId}`))
 
 const handleSubmit = async (event: Event) => {
   const form = event.target as HTMLFormElement
   const formData = new FormData(form)
   const name = formData.get("name")
 
-  await useFetch(`/api/category/sub-category/${categoryId}`, {
+  const { data: subCategory } = await useFetch(`/api/category/sub-category/${categoryId}`, {
     method: 'post',
     body: {
       name,
@@ -33,6 +42,7 @@ const handleSubmit = async (event: Event) => {
 
   form.reset()
   hide(modalId)
+  subCategories.value?.push(subCategory.value as ISubCategory)
 }
 </script>
 
@@ -60,4 +70,22 @@ const handleSubmit = async (event: Event) => {
       </ModalFooter>
     </form>
   </Modal>
+
+  <BaseTable v-if="subCategories?.length" class="w-full text-sm text-left text-gray-400 mt-8" striped>
+    <BaseTableHead class="text-xs text-gray-400 uppercase bg-gray-700">
+      <BaseTableHeadCell>Name</BaseTableHeadCell>
+      <BaseTableHeadCell><span class="sr-only">actions</span></BaseTableHeadCell>
+    </BaseTableHead>
+    <BaseTableBody>
+      <BaseTableRow v-for="item in subCategories" :key="item.name">
+        <BaseTableCell>
+          {{ item.name }}
+        </BaseTableCell>
+        <BaseTableCell class="flex justify-end items-center gap-2">
+          <EditRegisttryIcon class="text-blue-600" />
+          <RemoveRegisttryIcon class="text-red-500" />
+        </BaseTableCell>
+      </BaseTableRow>
+    </BaseTableBody>
+  </BaseTable>
 </template>
