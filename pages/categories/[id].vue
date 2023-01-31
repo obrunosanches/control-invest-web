129cf7858207d90cdcdaea2a39eeaa427184b452
Bruno Sanches <obrunosanches@gmail.com>
2023-01-30 21:46:34 -0300

    feat: add modal with form sub category

--- pages/categories/[id].vue
+++ pages/categories/[id].vue
@@ -3,21 +3,42 @@ import { useRoute } from 'vue-router'
 import type { Category as ICategory } from "@prisma/client";
 
 import Button from '~/components/TheButton.vue';
+import Modal from '~/components/Modal/TheModal.vue';
+import ModalHeader from '~/components/Modal/TheModal.Header.vue';
+import ModalBody from '~/components/Modal/TheModal.Body.vue';
+import ModalFooter from '~/components/Modal/TheModal.Footer.vue';
+import FormInput from '~/components/Form/FormInput.vue';
 
 // TODO: Use pinia to control store
-
 const route = useRoute()
-const { data: catogory } = await useAsyncData<ICategory>('categoryByID', () => $fetch(`/api/category/${route.params.id}`))
+const { id: categoryId } = route.params
+const modalId: string = "defaultModal"
+
+const { data: catogory } = await useAsyncData<ICategory>('categoryByID', () => $fetch(`/api/category/${categoryId}`))
 </script>
 
 <template>
   <h2 class="text-2xl">{{ catogory?.name }}</h2>
 
   <div class="flex justify-end">
-    <NuxtLink to="#">
-      <Button type="button" color="default">
-        Adicionar
-      </Button>
-    </NuxtLink>
+    <Button type="button" color="default" v-modal-show="modalId">
+      Adicionar
+    </Button>
   </div>
+
+  <Modal ref="modalElement" :target="modalId" position="top-center" class="mt-16">
+    <ModalHeader :target="modalId" title="Cadastrar sub categoria" />
+    <form novalidate>
+      <ModalBody :hasTitle="true">
+        <fieldset>
+          <FormInput name="name" label="Descrição" />
+        </fieldset>
+      </ModalBody>
+      <ModalFooter>
+        <Button type="button" color="default">
+          Confirmar
+        </Button>
+      </ModalFooter>
+    </form>
+  </Modal>
 </template>
