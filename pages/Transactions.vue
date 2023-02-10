936b885e74a289bcc5792df6b01fc7349aeab719
obrunosanches <obrunosanches@gmail.com>
2023-02-09 21:52:30 -0300

    feat: adjust modal with form transaction

--- pages/Transactions.vue
+++ pages/Transactions.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import type { Account as IAccount, Category as ICategory, SubCategory as ISubCategory } from "@prisma/client";
+
+
 import { hide } from '~/plugins/modal';
 
 import Button from '~/components/TheButton.vue';
@@ -11,12 +14,27 @@ import Select from '~/components/Form/FormSelect.vue';
 
 const transactionModalTarget: string = "accountModal"
 
+const { data: accounts } = await useAsyncData<IAccount[]>('account', () => $fetch('/api/account'))
+const { data: categories } = await useAsyncData<ICategory[]>('category', () => $fetch(`/api/category`))
+const subCategories = ref<ISubCategory[]>([])
+
 const handleSubmit = async (event: Event): Promise<void> => {
   const form = event.target as HTMLFormElement
   const formData = Object.fromEntries(new FormData(form))
 
+  console.log('formData', formData)
+
   form.reset()
   hide(transactionModalTarget)
+}
+
+const selectSubCategory = async (event: Event) => {
+  const category = event.target as HTMLSelectElement
+  const categoryId = category.value
+
+  const { data } = await useFetch<ISubCategory[]>(`/api/category/sub-category/${categoryId}`)
+
+  subCategories.value = data.value as ISubCategory[]
 }
 </script>
 
@@ -35,17 +53,7 @@ const handleSubmit = async (event: Event): Promise<void> => {
     <ModalHeader :target="transactionModalTarget" title="Cadastrar transação" />
     <form id="account" novalidate @submit.prevent="handleSubmit">
       <ModalBody :hasTitle="true">
-        <fieldset class="flex-auto">
-          <FormInput name="description" label="Descrição" />
-        </fieldset>
-        
-        <div class="flex mt-6 gap-4 items-end">
-          <fieldset class="basis-2/4">
-            <Select name="accountId" label="Conta">
-              <option selected>Selecione a conta</option>
-            </Select>
-          </fieldset>
-
+        <div class="flex gap-4 items-end">
           <fieldset class="flex-1">
             <FormInput name="value" label="Valor" />
           </fieldset>
@@ -53,18 +61,36 @@ const handleSubmit = async (event: Event): Promise<void> => {
           <fieldset class="flex-1">
             <FormInput name="date" type="date" label="Data" />
           </fieldset>
+
+          <fieldset class="basis-2/4">
+            <Select name="accountId" label="Conta">
+              <option v-for="{ id, name } in accounts" :key="id" :value="id">
+                {{ name }}
+              </option>
+            </Select>
+          </fieldset>
         </div>
+
+        <fieldset class="flex-auto mt-6">
+          <FormInput name="description" label="Descrição" />
+        </fieldset>
 
         <div class="flex mt-6 gap-4 items-end">
           <fieldset class="flex-1">
-            <Select name="categoryId" label="Categoria">
+            <Select name="categoryId" label="Categoria" @change="selectSubCategory">
               <option selected>Selecione a categoria</option>
+              <option v-for="{ id, name } in categories" :key="id" :value="id">
+                {{ name }}
+              </option>
             </Select>
           </fieldset>
 
           <fieldset class="flex-1">
             <Select name="subCategoryId" label="Sub Categoria">
               <option selected>Selecione a sub categoria</option>
+              <option v-for="{ id, name } in subCategories" :key="id" :value="id">
+                {{ name }}
+              </option>
             </Select>
           </fieldset>
         </div>
