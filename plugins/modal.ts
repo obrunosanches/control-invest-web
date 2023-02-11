05b77fb3a0f7680019b6634a4942e22c82240c76
Bruno Sanches <obrunosanches@gmail.com>
2023-02-10 22:52:17 -0300

    refactor: add custom event to control show and close modal

--- plugins/modal.ts
+++ plugins/modal.ts
@@ -1,34 +1,58 @@
-import { DirectiveBinding } from "vue";
+import { DirectiveBinding } from "vue"
 
-const getModalElementByTarget = (target: string) => document.getElementById(target) as HTMLDivElement;
+const getModalElementByTarget = (target: string) => document.getElementById(target) as HTMLDivElement
 
-export const show = (element: HTMLDivElement) => element.classList.remove("hidden");
+export const show = (element: HTMLDivElement) => {
+  if (element.classList.contains('hidden')) {
+    const onShowModal = new CustomEvent('on-show-modal', {
+      detail: {
+        target: element.getAttribute('id')
+      }
+    })
+
+    window.dispatchEvent(onShowModal)
+    element.classList.toggle("hidden")
+  }
+}
+
 export const hide = (target: string) => {
-  const $modalContainerElement = getModalElementByTarget(target);
-  $modalContainerElement.classList.add("hidden");
+  const $modalContainerElement = getModalElementByTarget(target)
+  
+  if (!$modalContainerElement.classList.contains('hidden')) {
+    const onCloseModal = new CustomEvent('on-close-modal', {
+      detail: {
+        target
+      }
+    })
+
+    $modalContainerElement.classList.toggle("hidden")
+    window.dispatchEvent(onCloseModal)
+  }
 }
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.directive("modal-show", (element: HTMLButtonElement, binding: DirectiveBinding) => {
     element.addEventListener("click", () => {
-      const $modalContainerElement = getModalElementByTarget(binding.value);
+      const $modalContainerElement = getModalElementByTarget(binding.value)
       show($modalContainerElement)
-    });
-  });
+    })
+  })
 
   nuxtApp.vueApp.directive("modal-hide", (element: HTMLButtonElement, binding: DirectiveBinding) => {
     const target = binding.value
-    const $modalContainerElement = getModalElementByTarget(target);
-    const $modalElemnt = $modalContainerElement.querySelector('[role="dialog"]') as HTMLDivElement;
+    const $modalContainerElement = getModalElementByTarget(target)
+    const $modalElemnt = $modalContainerElement.querySelector('[role="dialog"]') as HTMLDivElement
 
     $modalElemnt?.addEventListener("click", (element) => {
-      (element.target === $modalElemnt) && hide(target);
-    });
+      (element.target === $modalElemnt) && hide(target)
+    })
 
     $modalContainerElement.addEventListener("keydown", (event: KeyboardEvent) => {
-      (event.key === "Escape") && hide(target);
-    });
+      (event.key === "Escape") && hide(target)
+    })
 
-    element.addEventListener("click", () => hide(target));
-  });
-});
+    element.addEventListener("click", () => {
+      hide(target)
+    })
+  })
+})
