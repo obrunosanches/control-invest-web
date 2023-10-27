a39d0bd1c57fcea1d760a9e62cc71247ce69acd9
obrunosanches <obrunosanches@gmail.com>
2023-10-27 15:28:23 -0300

    feat: add layout default

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,38 @@
+<script setup lang="ts">
+useHead({
+  title: 'Control Invest',
+  bodyAttrs: {
+    class: 'bg-gray-900'
+  }
+})
+</script>
+
 <template>
-  <main class="w-full p-5">
-    <router-view></router-view>
-  </main>
+  <div class="h-screen flex">
+    <div class="shrink-0 w-48">
+      <aside
+          class="h-full w-48 fixed border-r border-b-0 bg-gray-800 border-gray-700"
+          aria-label="Sidebar"
+      >
+        <ul class="px-3 py-4 space-y-2">
+          <sidebar-item route="/">
+            Dashboard
+          </sidebar-item>
+          <sidebar-item route="/transactions">
+            Transações
+          </sidebar-item>
+          <sidebar-item route="/categories">
+            Categorias
+          </sidebar-item>
+          <sidebar-item route="/accounts">
+            Contas
+          </sidebar-item>
+        </ul>
+      </aside>
+    </div>
+
+    <main class="p-5 w-full">
+      <router-view></router-view>
+    </main>
+  </div>
 </template>
---
+++ pages/accounts.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 class="text-gray-300 text-3xl font-bold">
+    Contas
+  </h1>
+</template>
