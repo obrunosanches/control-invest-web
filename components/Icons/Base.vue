74a530219b3045c550e5f07f5cec544ddfe42062
obrunosanches <obrunosanches@gmail.com>
2023-10-27 17:40:21 -0300

    feat: add base icon

---
+++ components/Icons/Base.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import classNames from 'classnames'
+
+const attrs = useAttrs()
+const baseClasses = 'flex-shrink-0 w-6 h-6 transition duration-75'
+
+const className = computed(() => {
+  return classNames(baseClasses, {
+    [String(attrs.class)]: useAttrs().class
+  })
+})
+</script>
+
+<template>
+  <svg
+    :class="className"
+    v-bind="$attrs"
+    aria-hidden="true"
+    stroke="currentColor"
+    stroke-width="1.5"
+    fill="none"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <slot />
+  </svg>
+</template>
+
