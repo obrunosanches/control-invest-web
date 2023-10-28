4fc31a95611583babdc488297c67d1e5b7bd169b
obrunosanches <obrunosanches@gmail.com>
2023-10-28 13:33:37 -0300

    feat: add server connect mongodb

---
+++ server/database/connect.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client'
+
+let prisma: PrismaClient
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+  prisma.$connect()
+} else {
+  if (!global.__db) {
+    global.__db = new PrismaClient()
+    global.__db.$connect()
+  }
+  
+  prisma = global.__db
+}
+
+export { prisma }
