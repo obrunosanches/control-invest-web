2a0326979385a1d8ec3a7a8008e11028ade830f4
Bruno Sanches <obrunosanches@gmail.com>
2023-10-28 18:57:43 -0300

    refactor: adjust cmoponents form with formkit

--- components/Form/Input.vue
+++ components/Form/Input.vue
@@ -1,26 +1,10 @@
-<script setup lang="ts">
-import classNames from "classnames"
-
-const props = defineProps({
-  label: {
-    type: String
-  }
-})
-
-const idElement = String(useAttrs().name) ?? ''
-const baseClasses = 'border rounded w-full p-2.5 text-sm bg-gray-600 border-gray-500 placeholder-gray-400 text-white'
-const classes = computed(() => {
-  return classNames(
-    baseClasses, {
-      [String(useAttrs().class)]: useAttrs().class
-    })
-})
-</script>
-
 <template>
-  <form-label :for="idElement">
-    {{ props.label }}
-  </form-label>
-
-  <input :id="idElement" :class="classes" autocomplete="off" v-bind="$attrs" />
+  <FormKit
+    label-class="block mb-2 text-sm font-medium dark:text-gray-300"
+    outer-class="w-full"
+    input-class="w-full border rounded p-2.5 text-sm bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
+    help-class="text-xs text-gray-500"
+    autocomplete="off"
+    v-bind="useAttrs()"
+  />
 </template>
