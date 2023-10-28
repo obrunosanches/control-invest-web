880e52615ebee434f49855a79077adeb1e6e68fc
Bruno Sanches <obrunosanches@gmail.com>
2023-10-28 13:35:30 -0300

    feat: add server api accounts

---
+++ server/api/accounts/index.get.ts
@@ -0,0 +1,9 @@
+import { prisma } from '~/server/database/connect'
+
+export default defineEventHandler(() => {
+  return prisma.account.findMany({
+    include: {
+      accountType: true
+    }
+  })
+})
