29600d865a8c46bb74e75e43a9bf1358ab026cce
obrunosanches <obrunosanches@gmail.com>
2023-10-31 16:16:17 -0300

    refactor: modify to show modal client olnly

--- pages/accounts.vue
+++ pages/accounts.vue
@@ -18,7 +18,7 @@ const { accounts } = storeToRefs(accountStore)
 
 const modalTarget = 'main-modal'
 const accountFormId = 'account-form'
-const accountSelected = ref<AccountWithType | null>(null)
+const accountSelected = ref<AccountWithType>(null)
 
 onBeforeMount(async () => {
   await fetchAccounts()
@@ -72,6 +72,7 @@ const handleSubmit = async (payload): Promise<void> => {
       <form-kit
         type="button"
         label="Nova conta"
+        @click="handleSelectAccount(null)"
         v-show-modal="modalTarget"
         input-class="bg-purple-700 hover:bg-violet-600 text-white font-bold py-2 px-4 rounded"
       />
@@ -82,66 +83,69 @@ const handleSubmit = async (payload): Promise<void> => {
     </div>
   </main>
 
-  <base-modal ref="modalElement" :target="modalTarget" class="mt-16">
-    <template #header>
-      <h3 class="text-xl font-medium text-white">
-        Nova conta
-      </h3>
-      <form-kit
-        type="button"
-        v-close-modal="modalTarget"
-        input-class="text-sm ml-auto flex items-center hover:text-gray-300 text-gray-50"
-      >
-        <icons-close class="h-4 w-4" viewBox="0 0 20 20" />
-      </form-kit>
-    </template>
+  <client-only>
+    <base-modal ref="modalElement" :target="modalTarget" class="mt-16">
+      <template #header>
+        <h3 class="text-xl font-medium text-white">
+          Nova conta
+        </h3>
+        <form-kit
+          type="button"
+          v-close-modal="modalTarget"
+          input-class="text-sm ml-auto flex items-center hover:text-gray-300 text-gray-50"
+        >
+          <icons-close class="h-4 w-4" viewBox="0 0 20 20" />
+        </form-kit>
+      </template>
 
-    <template #body>
-      <form-kit
-        :id="accountFormId"
-        type="form"
-        :actions="false"
-        @submit="handleSubmit"
-        :incomplete-message="false"
-      >
-        <div class="p-6">
-          <div class="flex gap-4 items-end">
-            <form-input
-              type="text"
-              name="name"
-              label="Nome"
-              validation="required:trim"
-            />
-            <form-input
-              type="text"
-              name="initialBalance"
-              label="Valor inicial"
-            />
-          </div>
-          <div class="mt-6">
-            <form-select
-              name="accountTypeId"
-              label="Tipo de conta"
-              :options="[
-                { label: 'Selecione o tipo', value: ''},
-                ...accountTypesOptions,
-              ]"
-              validation="required:trim"
-            />
-            <div class="error-message">
-              <FormKitMessages :node="input?.name" />
+      <template #body>
+        <form-kit
+          :id="accountFormId"
+          type="form"
+          :actions="false"
+          @submit="handleSubmit"
+          :incomplete-message="false"
+          v-model="accountSelected"
+        >
+          <div class="p-6">
+            <div class="flex gap-4 items-end">
+              <form-input
+                type="text"
+                name="name"
+                label="Nome"
+                validation="required:trim"
+              />
+              <form-input
+                type="text"
+                name="initialBalance"
+                label="Valor inicial"
+              />
+            </div>
+            <div class="mt-6">
+              <form-select
+                name="accountTypeId"
+                label="Tipo de conta"
+                :options="[
+                    { label: 'Selecione o tipo', value: ''},
+                    ...accountTypesOptions,
+                  ]"
+                validation="required:trim"
+              />
+              <div class="error-message">
+                <FormKitMessages :node="input?.name" />
+              </div>
             </div>
           </div>
-        </div>
 
-        <section class="p-6 rounded-b border-t border-gray-600 text-right">
-          <form-kit
-            type="submit"
-            input-class="bg-purple-700 hover:bg-violet-600 text-white font-bold py-2 px-4 rounded"
-            label="Confirmar"
-          />
-        </section>
-      </form-kit>
-    </template>
-  </base-modal>
+          <section class="p-6 rounded-b border-t border-gray-600 text-right">
+            <form-kit
+              type="submit"
+              input-class="bg-purple-700 hover:bg-violet-600 text-white font-bold py-2 px-4 rounded"
+              label="Confirmar"
+            />
+          </section>
+        </form-kit>
+      </template>
+    </base-modal>
+  </client-only>
 </template>
