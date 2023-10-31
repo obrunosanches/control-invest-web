69d406289d7db375bba64692b1d68ac6a0ce49ea
Bruno Sanches <obrunosanches@gmail.com>
2023-10-31 16:19:15 -0300

    refactor: add placeholder account card

--- pages/accounts.vue
+++ pages/accounts.vue
@@ -78,8 +78,58 @@ const handleSubmit = async (payload): Promise<void> => {
       />
     </div>
 
-    <div v-for="{ name } in accounts">
-      <span class="text-white">{{ name }}</span>
+    <div class="grid grid-cols-2 grid-rows-1 gap-4 mt-6">
+      <div
+        v-for="account in accounts"
+        class="rounded-2xl bg-white p-8"
+      >
+        <header class="flex">
+          <span class="flex-auto">{{ account.name }}</span>
+          <div class="flex gap-4">
+            <form-kit
+              type="button"
+              label="Editar"
+              input-class="text-green-600"
+              @click="handleSelectAccount(account)"
+              v-show-modal="modalTarget"
+            >
+              Editar
+            </form-kit>
+
+            <form-kit
+              type="button"
+              label="Remover"
+              input-class="text-red-600"
+              @click="handleSelectAccount(account)"
+              :ignore="false"
+            />
+          </div>
+        </header>
+
+        <div class="flex gap-4 mt-6">
+          <div class="basis-1/3">
+            <form-kit
+              type="button"
+              label="Adicionar receita"
+              input-class="w-full bg-transparent border rounded p-3"
+            />
+          </div>
+          <div class="basis-1/3">
+            <form-kit
+              type="button"
+              label="Adicionar depesa"
+              input-class="w-full bg-transparent border rounded p-3"
+            />
+          </div>
+          <div class="basis-1/3">
+            <form-kit
+              type="button"
+              label="Ver transações"
+              input-class="w-full bg-transparent border rounded p-3"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 
