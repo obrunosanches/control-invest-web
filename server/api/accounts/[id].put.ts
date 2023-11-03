e5631ab39503165ccc3cd22e6b3e070d6cdba806
Bruno Sanches <obrunosanches@gmail.com>
2023-11-03 16:49:55 -0300

    refactor: remove unnecessary code

--- server/api/accounts/[id].put.ts
+++ server/api/accounts/[id].put.ts
@@ -1,7 +1,5 @@
 import { prisma } from "~/server/database/connect"
 
-import type { Account } from "@prisma/client"
-
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
   
