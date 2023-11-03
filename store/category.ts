46d3e3343f864d26777fc267822cfd84f54e3293
Bruno Sanches <obrunosanches@gmail.com>
2023-11-03 17:39:06 -0300

    feat(SERVER): create category

--- store/category.ts
+++ store/category.ts
@@ -1,5 +1,7 @@
 import { defineStore } from "pinia"
+
 import type { Category, CategoryType } from "@prisma/client"
+import { useCategoryTypeStore } from "~/store/categoryType"
 
 export interface CategoryWithType extends Category {
   categoryType: CategoryType
@@ -15,7 +17,38 @@ export const useCategoryStore = defineStore('categoryStore', {
   }),
   actions: {
     async fetchCategoriesByType(categoryTypeId) {
-      await $fetch<CategoryType[]>(`/api/category/${categoryTypeId}`)
+      try {
+        this.categories = await $fetch<CategoryType[]>(`/api/category/${categoryTypeId}`)
+        
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    async createOrUpdateCategory(category: Category) {
+      const { categoryTypes } = useCategoryTypeStore()
+      const apiUrl = `/api/category${category.id ? `/${category.id}` : ''}`
+      
+      try {
+        const response = await $fetch<Category>(apiUrl, {
+          method: category.id ? 'PUT' : 'POST',
+          body: {
+            name: category.name,
+            typeId: category.typeId
+          }
+        })
+        
+        if (category.id) {
+          return await this.fetchCategoriesByType(category.typeId)
+        }
+        
+        const categoryType = categoryTypes.find(
+          type => type.id === response.typeId
+        )
+        
+        this.categories.push({ ...response, categoryType })
+      } catch (error) {
+        console.error(error)
+      }
     }
   }
 })
