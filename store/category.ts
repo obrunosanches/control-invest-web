9d76783186d52dc18a1769b48d2f6b2f7f47c15c
Bruno Sanches <obrunosanches@gmail.com>
2023-11-03 16:51:38 -0300

    feat(SERVER): list categories

---
+++ store/category.ts
@@ -0,0 +1,21 @@
+import { defineStore } from "pinia"
+import type { Category, CategoryType } from "@prisma/client"
+
+interface CategoryWithType extends Category {
+  categoryType: CategoryType
+}
+
+interface State {
+  categories: CategoryWithType[]
+}
+
+export const useCategoryStore = defineStore('categoryStore', {
+  state: (): State => ({
+    categories: []
+  }),
+  actions: {
+    async fetchCategoriesByType(categoryTypeId) {
+      await $fetch<CategoryType[]>(`/api/category/${categoryTypeId}`)
+    }
+  }
+})
