dfeaa31b45ac51afdda6d2cc9eb13c3e48caa5a0
obrunosanches <obrunosanches@gmail.com>
2023-11-04 21:31:37 -0300

    feat: table component

---
+++ components/Base/Table.vue
@@ -0,0 +1,13 @@
+<template>
+  <table v-bind="useAttrs()">
+    <thead class="text-gray-400 uppercase">
+      <slot name="head" />
+    </thead>
+    <tbody>
+      <slot name="body" />
+    </tbody>
+    <tfoot class="text-gray-400 uppercase">
+      <slot name="foot" />
+    </tfoot>
+  </table>
+</template>
---
+++ components/Base/Table/HeadCell.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const baseClassesHeadCell = 'px-6 py-4 text-xs uppercase border-b border-white/[.15]'
+</script>
+
+<template>
+  <th :class="baseClassesHeadCell">
+    <slot />
+  </th>
+</template>
