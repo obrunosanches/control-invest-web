288c97019dc7bb09649d9e12b9689984c78809b7
Bruno Sanches <obrunosanches@gmail.com>
2023-11-04 21:31:37 -0300

    feat: table component

---
+++ components/Base/Table/BodyCell.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const baseClassesBodyCell = 'px-6 py-6 last:text-right border-b border-white/[.15]'
+</script>
+
+<template>
+  <td :class="baseClassesBodyCell">
+    <slot />
+  </td>
+</template>
---
+++ components/Base/Table/FootCell.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const baseClassesFootCell = 'px-6 py-4 last:text-right'
+</script>
+
+<template>
+  <td :class="baseClassesFootCell">
+    <slot />
+  </td>
+</template>
