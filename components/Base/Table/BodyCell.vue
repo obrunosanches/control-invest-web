6ced1915fd5d7d386c0cba3c2d8126d7dcda9bad
obrunosanches <obrunosanches@gmail.com>
2023-11-06 16:17:34 -0300

    refactor: adjust table component

--- components/Base/Table/BodyCell.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-const baseClassesBodyCell = 'px-6 py-6 last:text-right border-b border-white/[.15]'
-</script>
-
-<template>
-  <td :class="baseClassesBodyCell">
-    <slot />
-  </td>
-</template>
