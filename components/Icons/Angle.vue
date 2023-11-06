414e4a2459e70fd3c75d4e96d7d0c368164f2c5f
Bruno Sanches <obrunosanches@gmail.com>
2023-11-06 19:46:26 -0300

    refactor: adjust icon angle

--- components/Icons/Angle.vue
+++ components/Icons/Angle.vue
@@ -8,6 +8,7 @@ const props = defineProps<{
   <icons-base
     class="text-gray-50 hover:text-gray-300"
     :viewBox="['left', 'right'].includes(props.rotate ?? 'left') ? '0 0 8 14' : '0 0 14 8'"
+    v-bind="useAttrs()"
   >
     <path v-if="rotate === 'left'" d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13" />
     <path v-if="props.rotate === 'up'" d="M13 7 7.674 1.3a.91.91 0 0 0-1.348 0L1 7" />
