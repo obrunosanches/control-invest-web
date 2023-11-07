577a07b31c89ef53965f77409f8391afef14bd4f
obrunosanches <obrunosanches@gmail.com>
2023-11-07 15:54:48 -0300

    refactor: add month year component in template

--- pages/transactions.vue
+++ pages/transactions.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+import { useTransactionStore } from "~/store/transaction"
+
+import type { DateSelected } from "~/types"
+
+const { fetchTransaction } = useTransactionStore()
+
+const handleDateSelected = async (event: DateSelected) => {
+  await fetchTransaction(event.month, event.year)
+}
+</script>
+
 <template>
-  <month-year-selected @handle-click-date-select="dateSelected => console.log('dateSelected', dateSelected)" />
+  <month-year-selected @handle-click-date-select="handleDateSelected" />
 </template>
