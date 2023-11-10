8b2e25e8a0559c5e9aa8122ffbce8e822bfe5afa
obrunosanches <obrunosanches@gmail.com>
2023-11-10 16:42:57 -0300

    chore: config font default

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
     "./app.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Manrope', 'sans-serif']
+      }
+    }
   },
   plugins: [],
 }
