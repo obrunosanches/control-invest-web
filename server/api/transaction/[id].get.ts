61c51582fa19f3a62d79c4ad254f44e007e49ada
Bruno Sanches <obrunosanches@gmail.com>
2023-11-12 21:58:22 -0300

    feat(PRISMA): add set balance extension

---
+++ server/api/transaction/[id].get.ts
@@ -0,0 +1,15 @@
+import { prisma } from "~/server/database/connect"
+
+export default defineEventHandler((event) => {
+  return prisma.transaction.findUnique({
+    where: {
+      id: event.context.params.id
+    },
+    include: {
+      type: true,
+      category: true,
+      subCategory: true,
+      account: true
+    }
+  })
+})
--- server/database/connect.ts
+++ server/database/connect.ts
@@ -1,13 +1,14 @@
 import { PrismaClient } from '@prisma/client'
+import setBalance from "~/server/extensions/setBalance"
 
 let prisma: PrismaClient
 
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
+  prisma = new PrismaClient().$extends([setBalance])
   prisma.$connect()
 } else {
   if (!global.__db) {
-    global.__db = new PrismaClient()
+    global.__db = new PrismaClient().$extends([setBalance])
     global.__db.$connect()
   }
   
