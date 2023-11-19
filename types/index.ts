cd8ab4d0611954f8a200b76786dab2edbb6a98cf
Bruno Sanches <obrunosanches@gmail.com>
2023-11-19 18:35:11 -0300

    feat(STORE): create transfer transaction

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,5 @@
+export type TransactionTypesOptions = 'expenses' | 'earnings' | 'transfer' | 'transaction'
+
 export type TransactionTypeSlug = 'expenses' | 'earnings' | 'expenses-transfer' | 'earnings-transfer'
 
 export type ItemActionType = 'create' | 'update' | 'delete'
