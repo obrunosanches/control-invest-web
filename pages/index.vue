d02d0bfa9adfb80c73a2c33cd22ea12a9d316998
obrunosanches <obrunosanches@gmail.com>
2023-12-13 18:53:06 -0300

    refactor: pendencies

--- pages/index.vue
+++ pages/index.vue
@@ -2,4 +2,60 @@
   <h1 class="text-3xl font-bold">
     Dashboard
   </h1>
+
+  <div class="mt-8">
+    <div class="mt-8">
+      <span class="font-medium mt-8">Pendências</span>
+      <ul class="mt-4 space-y-4">
+        <li class="flex gap-2">
+          <img src="https://cdn-icons-png.flaticon.com/512/4436/4436481.png" width="24" height="24" />
+          Corrigir filtro ao editar e remover transações
+        </li>
+        <li class="flex gap-2">
+          <img src="https://cdn-icons-png.flaticon.com/512/4436/4436481.png" width="24" height="24" />
+          Deletar transfer por id
+        </li>
+        <li class="flex gap-2">
+          <img src="https://cdn-icons-png.flaticon.com/512/4436/4436481.png" width="24" height="24" />
+          Adicinoar observação ao schema da transação
+        </li>
+        <li class="flex gap-2">
+          <img src="https://cdn-icons-png.flaticon.com/512/4436/4436481.png" width="24" height="24" />
+          Edit transfer (Não está exibindo o formulário correto)
+        </li>
+        <li class="flex gap-2">
+          <img src="https://cdn-icons-png.flaticon.com/512/4436/4436481.png" width="24" height="24" />
+          Modificar modal ao remover transferência
+        </li>
+        <li class="flex gap-2">
+          <img src="https://cdn-icons-png.flaticon.com/512/4436/4436481.png" width="24" height="24" />
+          Filtrar transações por tipo
+        </li>
+        <li class="flex gap-2">
+          <img src="https://cdn-icons-png.flaticon.com/512/4436/4436481.png" width="24" height="24" />
+          Filtrar transações por transferências
+        </li>
+        <li class="flex gap-2">
+          <img src="https://cdn-icons-png.flaticon.com/512/4159/4159545.png" width="24" height="24" />
+          Verificar editar quando salva uma conta e modifica para outra
+        </li>
+        <li class="flex gap-2">
+          <img src="https://cdn-icons-png.flaticon.com/512/4159/4159545.png" width="24" height="24" />
+          Ajustar input currency form transaction
+        </li>
+        <li class="flex gap-2">
+          <img src="https://cdn-icons-png.flaticon.com/512/4159/4159545.png" width="24" height="24" />
+          Ajustar foco modal para transferências
+        </li>
+        <li class="flex gap-2">
+          <img src="https://cdn-icons-png.flaticon.com/512/4159/4159545.png" width="24" height="24" />
+          Limitar criar novos itens baseado na conta selectionada (tela de accounts)
+        </li>
+        <li class="flex gap-2">
+          <img src="https://cdn-icons-png.flaticon.com/512/4159/4159545.png" width="24" height="24" />
+          Enviar para tela de transações filtrado pela conta selecionada
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
