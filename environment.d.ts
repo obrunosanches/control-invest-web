a5f05b7a36fd4e8cf5936a281de15eac9f708223
obrunosanches <obrunosanches@gmail.com>
2024-01-12 22:31:33 -0300

    feat: create helper fetch request

--- environment.d.ts
+++ environment.d.ts
@@ -2,5 +2,6 @@ declare namespace NodeJS {
   interface ProcessEnv {
     [key: string]: string | undefined
     DATABASE_URL: string
+    NEXT_PUBLIC_API_BASE_URL: string
   }
 }
---
+++ src/server/request.ts
@@ -0,0 +1,8 @@
+export function request(path: string, init?: RequestInit) {
+  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL
+  const apiPrefix = '/api'
+  
+  const url = new URL(apiPrefix.concat(path), baseUrl)
+  
+  return fetch(url, init)
+}
