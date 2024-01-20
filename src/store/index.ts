f4956ce75efef285061a272289cda58157a280f1
obrunosanches <obrunosanches@gmail.com>
2024-01-19 22:53:19 -0300

    refactor: add global store

--- src/store/index.ts
+++ src/store/index.ts
@@ -12,19 +12,22 @@ import {
   SliceStateAccountTypes,
   type SliceStoreAccountTypes
 } from '@/store/slices/account-types'
+import { globalSlice, SliceActionsGlobal, SliceStateGlobal, SliceStoreGlobal } from '@/store/slices/global'
 
-export type StoreState = SliceStateAccounts & SliceStateAccountTypes
-export type StoreActions = SliceActionsAccounts & SliceActionsAccountTypes
-export type StoreProps = SliceStoreAccounts & SliceStoreAccountTypes
+export type StoreState = SliceStateAccounts & SliceStateAccountTypes & SliceStateGlobal
+export type StoreActions = SliceActionsAccounts & SliceActionsAccountTypes & SliceActionsGlobal
+export type StoreProps = SliceStoreAccounts & SliceStoreAccountTypes & SliceStoreGlobal
 
 export const useAppStore = create<StoreProps>()((...props) => ({
     state: {
       ...accountSlice(...props).state,
-      ...accountTypeSlice(...props).state
+      ...accountTypeSlice(...props).state,
+      ...globalSlice(...props).state
     },
     actions: {
       ...accountSlice(...props).actions,
-      ...accountTypeSlice(...props).actions
+      ...accountTypeSlice(...props).actions,
+      ...globalSlice(...props).actions
     }
   })
 )
