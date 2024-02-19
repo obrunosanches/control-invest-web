285ef99486ddbfa47b7fa4f3f41af39cc6f045f4
obrunosanches <obrunosanches@gmail.com>
2024-02-19 19:11:38 -0300

    refactor: remove middleware validation

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,16 +6,17 @@ export const config = {
 }
 
 export function middleware(request: NextRequest, context: Context) {
+  // TODO: improve validation routes with id
   const { pathname } = request.nextUrl
-  const validateCategoryTypeRouteWithId = ['/api/category/type/']
-  const validateRouteWithId = ['/api/account/', '/api/category/', '/api/transaction/', '/api/transfer/']
+  const validateCategoryTypeRouteWithId = ['/api/category/sub/', '/api/category/type/']
+  const validateRouteWithId = ['/api/account/', '/api/transaction/', '/api/transfer/']
   
   if (validateCategoryTypeRouteWithId.some(route => pathname.startsWith(route))) {
     const parts = pathname.split('/').filter(Boolean)
     const ids = parts.filter(Number)
     
     if (parts.length <= 4 && !ids.length) {
-      return NextResponse.json({ message: `invalid id parameter to ${pathname}` })
+      return NextResponse.json({ message: `invalid category type id parameter to ${pathname}` })
     }
     
     return NextResponse.next()
@@ -26,7 +27,7 @@ export function middleware(request: NextRequest, context: Context) {
     const ids = parts.filter(Number)
     
     if ((parts.length <= 3 && !ids.length) || (parts.length > 3 && ids.length < 2)) {
-      return NextResponse.json({ message: `invalid id parameter to ${pathname}` })
+      return NextResponse.json({ message: `invalid with id parameter to ${pathname}` })
     }
   }
   
