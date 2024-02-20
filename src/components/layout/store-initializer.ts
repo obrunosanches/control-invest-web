4ff339a08bb6b84e1b0000d8e4875552d945ef20
obrunosanches <obrunosanches@gmail.com>
2024-02-20 19:28:54 -0300

    refactor: load needed data on layout

---
+++ src/components/layout/store-initializer.ts
@@ -0,0 +1,23 @@
+'use client'
+
+import { type PropsWithChildren, useRef } from 'react'
+import { useCIStore } from '@/hooks/control-invest-store-provider'
+import { CIState } from '@/types/store'
+
+function StoreInitializer({ children, ...state }: PropsWithChildren & Partial<CIState> ) {
+  const initialized = useRef(false)
+  const store = useCIStore(store => store)
+  
+  if (!initialized.current) {
+    state.accountTypes && store.actions.setAccountTypes(state.accountTypes)
+    state.accounts && store.actions.setAccounts(state.accounts)
+    state.categories && store.actions.setCategories(state.categories)
+    state.transactionTypes && store.actions.setTransactionTypes(state.transactionTypes)
+    
+    initialized.current = true
+  }
+  
+  return children
+}
+
+export default StoreInitializer
