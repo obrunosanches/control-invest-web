b548e530eaa2731d3deb9274118a48b2d7961289
obrunosanches <obrunosanches@gmail.com>
2024-02-24 14:02:52 -0300

    refactor: improve code

--- src/components/layout/month-year-selected.tsx
+++ src/components/layout/month-year-selected.tsx
@@ -9,7 +9,7 @@ import { useCallback, useEffect, useState } from 'react'
 import { firstLetterUppercase } from '@/utils/srting'
 
 const DATE_FORMAT_OPTIONS = { month: 'long', year: 'numeric' } as Intl.DateTimeFormatOptions
-const BASE_BUTTON_CLASS = 'border border-2 border-purple-700 hover:border-purple-500 '+
+export const BASE_BUTTON_CLASS = 'border border-2 border-purple-700 hover:border-purple-500 '+
   ' text-purple-700 text-sm text-purple-700 hover:text-purple-500 font-bold font-medium' +
   ' py-2.5 px-5 rounded-full'
  
@@ -19,12 +19,10 @@ function MonthYearSelected({ onDateSelected }: { onDateSelected: (month: string,
   const [shouldShowSelectDateByMonth, setShouldShowSelectDateByMonth] = useState(false)
   
   const handleShowMonths = useCallback(() => {
-    const options = shouldShowSelectDateByMonth
-      ? DATE_FORMAT_OPTIONS
-      : { year: 'numeric' } as Intl.DateTimeFormatOptions
-    
-    setDateTimeFormatOptions(options)
-    setShouldShowSelectDateByMonth(prevState => !prevState)
+    if (!shouldShowSelectDateByMonth) {
+      setDateTimeFormatOptions({ year: 'numeric' })
+      setShouldShowSelectDateByMonth(prevState => !prevState)
+    }
   }, [shouldShowSelectDateByMonth])
   
   const handleNavigateDate = useCallback((value: 'prev' | 'next' | null): void => {
@@ -36,24 +34,16 @@ function MonthYearSelected({ onDateSelected }: { onDateSelected: (month: string,
     }
     
     const currentDate = dateSelected
-    let newDate: number
-    
-    if (shouldShowSelectDateByMonth) {
-      const newValue = value === 'prev' ? currentDate.getFullYear() + 1 : currentDate.getFullYear() - 1
-      
-      newDate = currentDate.setFullYear(newValue)
-    } else {
-      const newValue = value === 'prev' ? currentDate.getMonth() + 1 : currentDate.getMonth() - 1
-      
-      newDate = currentDate.setMonth(newValue)
-    }
+    const newYear = value === 'next' ? currentDate.getFullYear() + 1 : currentDate.getFullYear() - 1
+    const newMonth = value === 'next' ? currentDate.getMonth() + 1 : currentDate.getMonth() - 1
+    const newDate = shouldShowSelectDateByMonth ? currentDate.setFullYear(newYear) : currentDate.setMonth(newMonth)
     
     setDateSelected(new Date(newDate))
   }, [dateSelected, shouldShowSelectDateByMonth])
   
   useEffect(() => {
     onDateSelected(
-      dateSelected.getMonth().toString(),
+      monthsByLocale('en')[dateSelected.getMonth()].toLowerCase(),
       dateSelected.getFullYear().toString()
     )
   }, [dateSelected, onDateSelected])
